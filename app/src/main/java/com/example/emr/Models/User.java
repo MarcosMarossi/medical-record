4d5d3ed400567c24e6b5031d0a61f9fd48b61d6c
Vitor Iuri <buenovitor51@gmail.com>
2019-11-19 21:36:31 -0300

    Change user model and change activity_qr_code.xml

--- app/src/main/java/com/example/emr/Models/User.java
+++ app/src/main/java/com/example/emr/Models/User.java
@@ -9,6 +9,24 @@ public class User {
     private String cpf;
     private String message;
     private String profile;
+    private String id;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public String getName() {
         return name;
@@ -82,4 +100,8 @@ public class User {
         this.password = password;
     }
 
+    @Override
+    public String toString() {
+        return email;
+    }
 }

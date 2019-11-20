947116d1c2e9b264277b918baf71f63157d10cd2
Vitor Iuri <buenovitor51@gmail.com>
2019-11-19 21:41:57 -0300

    Add some endpoints

--- app/src/main/java/com/example/emr/Services/DataService.java
+++ app/src/main/java/com/example/emr/Services/DataService.java
@@ -1,8 +1,10 @@
 package com.example.emr.Services;
 
+import com.example.emr.Models.Heartbeat;
 import com.example.emr.Models.Scheduling;
 import com.example.emr.Models.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -10,6 +12,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 
 public interface DataService {
 
@@ -30,4 +33,13 @@ public interface DataService {
 
     @POST("api/schedulling")
     Call<Scheduling> agendar(@Body Scheduling scheduling);
+
+    @GET("api/patients")
+    Call<ArrayList<User>> getAllPatients();
+
+    @GET("{mac}")
+    Call<Heartbeat> getBPM(@Path("mac") String mac);
+
+    @POST("misc/heartbeat")
+    Call<User> setBPMUser(@Body User user);
 }
